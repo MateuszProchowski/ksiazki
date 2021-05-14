25017c6617f6cbd228f7073d53e69e2b78094307
MateuszProchowski <mateuszprochowski8@gmail.com>
2021-05-14 13:18:03 +0200

    juz

---
+++ main.js
@@ -0,0 +1,79 @@
+const ok = document.getElementById('grid')
+
+const grid = document.createElement("div")
+grid.setAttribute('class' , 'grid')
+
+const d1 = document.createElement("div");
+d1.setAttribute( 'class' , 'item')
+
+const t1 = document.createElement("h1");
+t1.setAttribute( 'class' , 'tytuł1')
+t1.innerText = 'book6'
+
+const book6 = document.createElement("p");
+book6.setAttribute( 'class' , 'book1')
+book6.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse natus quos qui deserunt, earum totam amet dicta nobis minus tempora quaerat porro rem culpa saepe. Dolorem rerum cum illo perferendis!Praesentium neque eligendi eius minus quam consectetur, ratione facilis '
+
+const item7 = document.createElement("div");
+item7.setAttribute( 'class' , 'item')
+
+const tytuł7 = document.createElement("h1");
+tytuł7.setAttribute( 'class' , 'tytuł1')
+tytuł7.innerText = 'book6'
+
+
+const book7 = document.createElement("p");
+book7.setAttribute( 'class' , 'book1')
+book7.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse natus quos qui deserunt, earum totam amet dicta nobis minus tempora quaerat porro rem culpa saepe. Dolorem rerum cum illo perferendis!Praesentium neque eligendi eius minus quam consectetur, ratione facilis '
+
+const item8 = document.createElement("div");
+item8.setAttribute( 'class' , 'item')
+
+const tytuł8 = document.createElement("h1");
+tytuł8.setAttribute( 'class' , 'tytuł1')
+tytuł8.innerText = 'book6'
+
+const ikona8 = document.createElement("i");
+ikona8.setAttribute( 'class' , 'fa fa-close ikona1')
+
+const book8 = document.createElement("p");
+book8.setAttribute( 'class' , 'book1')
+book8.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse natus quos qui deserunt, earum totam amet dicta nobis minus tempora quaerat porro rem culpa saepe. Dolorem rerum cum illo perferendis!Praesentium neque eligendi eius minus quam consectetur, ratione facilis '
+
+
+
+const item9 = document.createElement("div");
+item9.setAttribute( 'class' , 'item')
+
+const tytuł9 = document.createElement("h1");
+tytuł9.setAttribute( 'class' , 'tytuł1')
+tytuł9.innerText = 'book6'
+
+const book9 = document.createElement("p");
+book9.setAttribute( 'class' , 'book1' )
+book9.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse natus quos qui deserunt, earum totam amet dicta nobis minus tempora quaerat porro rem culpa saepe. Dolorem rerum cum illo perferendis!Praesentium neque eligendi eius minus quam consectetur, ratione facilis '
+
+
+const item10 = document.createElement("div");
+item10.setAttribute( 'class' , 'item')
+
+const tytuł10 = document.createElement("h1");
+tytuł10.setAttribute( 'class' , 'tytuł1')
+tytuł10.innerText = 'book6'
+
+
+const book10 = document.createElement("p");
+book10.setAttribute( 'class' , 'book1')
+book10.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse natus quos qui deserunt, earum totam amet dicta nobis minus tempora quaerat porro rem culpa saepe. Dolorem rerum cum illo perferendis!Praesentium neque eligendi eius minus quam consectetur, ratione facilis '
+
+
+const item11 = document.createElement("div");
+item11.setAttribute( 'class' , 'item')
+
+const tytuł11 = document.createElement("h1");
+tytuł11.setAttribute( 'class' , 'tytuł1')
+tytuł11.innerText = 'book6'
+
+const book11 = document.createElement("p");
+book11.setAttribute( 'class' , 'book1')
+book11.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse natus quos qui deserunt, earum totam amet dicta nobis minus tempora quaerat porro rem culpa saepe. Dolorem rerum cum illo perferendis!Praesentium neque eligendi eius minus quam consectetur, ratione facilis '
